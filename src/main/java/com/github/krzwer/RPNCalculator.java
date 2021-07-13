c8c5e886396dc4351ae0056416b03e7e649d13a4
krzwier <krzwier@gmail.com>
2021-07-13 10:14:14 -0500

    Refactors for better readability, adds new test cases

--- src/main/java/com/github/krzwer/RPNCalculator.java
+++ src/main/java/com/github/krzwer/RPNCalculator.java
@@ -2,30 +2,54 @@ package com.github.krzwer;
 
 import java.util.Stack;
 
-/**
- * Hello world!
- *
- */
 public class RPNCalculator {
 
-    private Stack<Integer> argStack = new Stack();
+   private Stack<Integer> argStack = new Stack<Integer>();
 
-    public int calculate(String input) {
-        String[] items = input.split(" ");
-        for (int i = 0; i < items.length; i++) {
-            if (items[i].matches("\\d+")) {
-                int num = Integer.parseInt(items[i]);
-                argStack.push(num);
-            } else if (items[i].equals("+")) {
-                int num1 = argStack.pop();
-                int num2 = argStack.pop();
-                argStack.push(num1 + num2);
-            }
-        }
-        if (input == "") {
-            return 0;
-        }
-        return argStack.pop();
+   public int calculate(String input) {
+      if (input == "") {
+         return 0;
+      }
+      String[] items = input.split(" ");
+      for (int i = 0; i < items.length; i++) {
+         if (items[i].matches("\\d+")) {
+            int num = Integer.parseInt(items[i]);
+            argStack.push(num);
+         } else if (items[i].equals("+")) {
+            argStack.push(add(getTopTwoNumbers()));
+         } else if (items[i].equals("-")) {
+            argStack.push(subtract(getTopTwoNumbers()));
+         } else if (items[i].equals("*")) {
+            argStack.push(multiply(getTopTwoNumbers()));
+         } else if (items[i].equals("/")) {
+            argStack.push(divide(getTopTwoNumbers()));
+         }
+      }
+      return argStack.pop();
+   }
 
-    }
+   private int[] getTopTwoNumbers() {
+      int[] nums = new int[2];
+      nums[1] = argStack.pop();
+      nums[0] = argStack.pop();
+      return nums;
+   }
+
+   private int add(int[] nums) {
+      return nums[0] + nums[1];
+   }
+
+   private int subtract(int[] nums) {
+      return nums[0] - nums[1];
+   }
+
+   private int multiply(int[] nums) {
+      return nums[0] * nums[1];
+   }
+
+   private int divide(int[] nums) {
+      return nums[0] / nums[1];
+   }
+
+
 }
--- src/test/java/com/github/krzwer/RPNCalculatorTest.java
+++ src/test/java/com/github/krzwer/RPNCalculatorTest.java
@@ -6,92 +6,67 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 
 import static org.junit.jupiter.api.Assertions.*;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 import java.util.stream.Stream;
-import org.junit.jupiter.params.provider.*;
 import org.junit.jupiter.params.provider.Arguments;
-
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import org.junit.jupiter.params.*;
-
 class RPNCalculatorTest {
 
-    private RPNCalculator rpn;
+   private RPNCalculator rpn;
 
-    @BeforeEach
-    void setUp() {
-        rpn = new RPNCalculator();
-    }
+   @BeforeEach
+   void setUp() {
+      rpn = new RPNCalculator();
+   }
 
-    @ParameterizedTest(name = "Return {1} for input \"{0}\"")
-    @MethodSource("input_to_expected")
-    void testCalculator(String input, int expected) {
-        assertEquals(expected, rpn.calculate(input));
-    }
+   @ParameterizedTest(name = "Return {1} for input \"{0}\"")
+   @MethodSource
+   void testCalculator(String input, int expected) {
+      assertEquals(expected, rpn.calculate(input));
+   }
 
-    static Stream<Arguments> input_to_expected() {
-        return Stream.of(
-           arguments("", 0), 
-           arguments("1", 1), 
-           arguments("3", 3),
-           arguments("1 1 +", 2),
-           arguments("1 3 +", 4),
-           arguments("3 4 5 + +", 12)
-           );
-    }
+   static Stream<Arguments> testCalculator() {
+      return Stream.of(
+            arguments("", 0),
+            arguments("1", 1),
+            arguments("3", 3),
+            arguments("1 1 +", 2),
+            arguments("1 3 +", 4),
+            arguments("8 7 +", 15),
+            arguments("3 4 5 + +", 12),
+            arguments("99 11 + 8 7 + +", 125),
+            arguments("1 3 -", -2),
+            arguments("8 3 -", 5),
+            arguments("33 3 - 10 6 - -", 26),
+            arguments("1 3 *", 3),
+            arguments("4 7 *", 28),
+            arguments("4 7 * 5 2 * *", 280),
+            arguments("10 2 /", 5),
+            arguments("36 9 /", 4),
+            arguments("90 3 / 30 5 / /", 5),
+            arguments("3 4 - 5 +", 4),
+            arguments("12 4 / 1 -", 2),
+            arguments("12 4 1 - /", 4),
+            arguments("15 7 1 1 + - / 3 * 2 1 1 + + -", 5));
+   }
 
-    @Nested
-    @DisplayName("Default Tests")
-    class DefaultTests {
+   @Nested
+   @DisplayName("Default Tests")
+   class DefaultTests {
 
-        @Test
-        @DisplayName("Empty test")
-        void contextLoads() {}
+      @Test
+      @DisplayName("Empty test")
+      void contextLoads() {}
 
-        @Test
-        @DisplayName("True equals true")
-        void true_equals_true() {
-            assertTrue(true);
-        }
-    }
+      @Test
+      @DisplayName("True equals true")
+      void true_equals_true() {
+         assertTrue(true);
+      }
+   }
 
-   //  @Nested
-   //  @DisplayName("RPNCalculator.calculate")
-   //  class calculateTest {
-   //      @Test
-   //      @DisplayName("Return 0 for empty expression")
-   //      void empty_returns_0() {
-   //          int actual = rpn.calculate("");
-   //          assertEquals(0, actual);
-   //      }
-
-   //      @Test
-   //      @DisplayName("Return 1 for input 1")
-   //      void input1_returns_1() {
-   //          int actual = rpn.calculate("1");
-   //          assertEquals(1, actual);
-   //      }
-
-   //      @Test
-   //      @DisplayName("Return 2 for input 1 1 +")
-   //      void input11plus_returns_2() {
-   //          int actual = rpn.calculate("1 1 +");
-   //          assertEquals(2, actual);
-   //      }
-
-   //      @Test
-   //      @DisplayName("Return 12 for input 3 4 5 + +")
-   //      void input345plusplus_returns_12() {
-   //          int actual = rpn.calculate("3 4 5 + +");
-   //          assertEquals(12, actual);
-   //      }
-
-   //  }
 
 }
