20a666fc9746fb63fbf4168c321bd5956eabc80e
krzwier <krzwier@gmail.com>
2021-07-12 23:55:54 -0500

    Converts to parameterized testing

--- src/test/java/com/github/krzwer/RPNCalculatorTest.java
+++ src/test/java/com/github/krzwer/RPNCalculatorTest.java
@@ -9,6 +9,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.provider.*;
+import org.junit.jupiter.params.provider.Arguments;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import org.junit.jupiter.params.*;
 
 class RPNCalculatorTest {
 
@@ -19,14 +29,30 @@ class RPNCalculatorTest {
         rpn = new RPNCalculator();
     }
 
+    @ParameterizedTest(name = "Return {1} for input \"{0}\"")
+    @MethodSource("input_to_expected")
+    void testCalculator(String input, int expected) {
+        assertEquals(expected, rpn.calculate(input));
+    }
+
+    static Stream<Arguments> input_to_expected() {
+        return Stream.of(
+           arguments("", 0), 
+           arguments("1", 1), 
+           arguments("3", 3),
+           arguments("1 1 +", 2),
+           arguments("1 3 +", 4),
+           arguments("3 4 5 + +", 12)
+           );
+    }
+
     @Nested
     @DisplayName("Default Tests")
     class DefaultTests {
 
         @Test
         @DisplayName("Empty test")
-        void contextLoads() {
-        }
+        void contextLoads() {}
 
         @Test
         @DisplayName("True equals true")
@@ -35,37 +61,37 @@ class RPNCalculatorTest {
         }
     }
 
-    @Nested
-    @DisplayName("RPNCalculator.calculate")
-    class calculateTest {
-        @Test
-        @DisplayName("Return 0 for empty expression")
-        void empty_returns_0() {
-            int actual = rpn.calculate("");
-            assertEquals(0, actual);
-        }
+   //  @Nested
+   //  @DisplayName("RPNCalculator.calculate")
+   //  class calculateTest {
+   //      @Test
+   //      @DisplayName("Return 0 for empty expression")
+   //      void empty_returns_0() {
+   //          int actual = rpn.calculate("");
+   //          assertEquals(0, actual);
+   //      }
 
-        @Test
-        @DisplayName("Return 1 for input 1")
-        void input1_returns_1() {
-            int actual = rpn.calculate("1");
-            assertEquals(1, actual);
-        }
+   //      @Test
+   //      @DisplayName("Return 1 for input 1")
+   //      void input1_returns_1() {
+   //          int actual = rpn.calculate("1");
+   //          assertEquals(1, actual);
+   //      }
 
-        @Test
-        @DisplayName("Return 2 for input 1 1 +")
-        void input11plus_returns_2() {
-            int actual = rpn.calculate("1 1 +");
-            assertEquals(2, actual);
-        }
+   //      @Test
+   //      @DisplayName("Return 2 for input 1 1 +")
+   //      void input11plus_returns_2() {
+   //          int actual = rpn.calculate("1 1 +");
+   //          assertEquals(2, actual);
+   //      }
 
-        @Test
-        @DisplayName("Return 12 for input 3 4 5 + +")
-        void input345plusplus_returns_12() {
-            int actual = rpn.calculate("3 4 5 + +");
-            assertEquals(12, actual);
-        }
+   //      @Test
+   //      @DisplayName("Return 12 for input 3 4 5 + +")
+   //      void input345plusplus_returns_12() {
+   //          int actual = rpn.calculate("3 4 5 + +");
+   //          assertEquals(12, actual);
+   //      }
 
-    }
+   //  }
 
 }
